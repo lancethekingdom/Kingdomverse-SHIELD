f2faf0e482cb013cb860f53b168b7f136ebea06f
Lance Tsang <lancetsang@Lances-MacBook-Pro-2.local>
2022-08-29 10:15:27 +0800

    chore: fixed test failed case, updated smart contract import removed erc20capped

--- __test__/specs/mint.spec.ts
+++ __test__/specs/mint.spec.ts
@@ -9,24 +9,29 @@ const chance = new Chance()
 
 describe('UNIT TEST: Shield Token - mint', () => {
   it('should throw error if contract is paused', async () => {
-    const [owner] = await ethers.getSigners()
-    const [token, , authSigner] = await deployShieldToken({ owner })
+    const [owner, authSigner, target] = await ethers.getSigners()
+    const [token] = await deployShieldToken({ owner, authSigner })
+
+    const mintAmount = chance.integer({ min: 1, max: 100 })
+
+    const msgHash = ethers.utils.solidityKeccak256(
+      ['address', 'address', 'uint256', 'uint256'],
+      [token.address, owner.address, UnitParser.toEther(mintAmount), 1],
+    )
+
+    const authedSig = await authSigner.signMessage(
+      ethers.utils.arrayify(msgHash),
+    )
 
     await token.connect(owner).pause()
 
     const paused = await token.paused()
 
-    const msgHash = ethers.utils.solidityKeccak256(
-      ['address', 'address', 'uint256', 'uint256'],
-      [token.address, owner.address, UnitParser.toEther(10), 1],
-    )
-    const sig = await authSigner.signMessage(msgHash)
-
     expect(paused).to.be.true
 
     return token
       .connect(owner)
-      .mint(owner.address, 0, 0, sig)
+      .mint(target.address, UnitParser.toEther(mintAmount), 1, authedSig)
       .then(() => assert.fail())
       .catch((err: any) => {
         assert.include(err.message, 'Pausable: paused')
@@ -44,7 +49,9 @@ describe('UNIT TEST: Shield Token - mint', () => {
       [token.address, owner.address, UnitParser.toEther(mintAmount), 1],
     )
 
-    const authedSig = await authSigner.signMessage(ethers.utils.arrayify(msgHash))
+    const authedSig = await authSigner.signMessage(
+      ethers.utils.arrayify(msgHash),
+    )
 
     const balanceBefore = await token.balanceOf(target.address)
 
