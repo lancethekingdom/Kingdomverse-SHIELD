94be928e2b396a8ed7e1275c95554d592f18a52f
lancethekingdom <lance@kingdomverse.xyz>
2022-10-05 09:45:23 +0800

    feat: added deployment script

---
+++ deploy/00_deploy_shield.ts
@@ -0,0 +1,22 @@
+import { HardhatRuntimeEnvironment } from 'hardhat/types'
+import { DeployFunction } from 'hardhat-deploy/types'
+
+const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
+  const { deployments, getNamedAccounts, network } = hre
+  const { deploy } = deployments
+
+  const { deployer } = await getNamedAccounts()
+
+  const res = await deploy('Shield', {
+    from: deployer,
+    args: [deployer],
+    log: true,
+    autoMine: true, // speed up deployment on local network (ganache, hardhat), no effect on live networks
+  })
+
+  console.log(
+    `Shield has been deployed to ${network.name} at ${res.address} with ${res.gasEstimates} gas`,
+  )
+}
+export default func
+func.tags = ['Shield']
